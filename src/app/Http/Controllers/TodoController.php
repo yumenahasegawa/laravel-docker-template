deeec8e057adaf9a628bbb6fda46b4d94b600e07
yumena.hasegawa <yumena.hasegawa@giztech-pro.com>
2024-06-24 18:41:01 +0900

    add:新規作成機能からDBにデータを保存する

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -20,4 +20,20 @@ class TodoController extends Controller
         // dd('新規作成画面のルート実行！');
         return view('todo.create'); // 追記
     }
+
+    public function store(Request $request)
+    {
+        // dd('新規作成のルート実行！');
+        $content = $request->input('content'); // 追記
+
+         // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+    $todo = new Todo(); 
+    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+    $todo->content = $content;
+    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo->save();
+
+    return redirect()->route('todo.index'); // 追記
+
+    }
 }
